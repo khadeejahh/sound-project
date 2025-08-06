96e8cafb4769c66dffbbcde724dd1a0c309ce1b2
khadeejahh <khadeejahjaved25@gmail.com>
2025-08-06 12:33:09 +0500

    Initial commit

---
+++ resources/views/user/youtubefeed.blade.php
@@ -0,0 +1,49 @@
+ <!-- Youtube Section Begin -->
+ <section class="youtube spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <h2>Latest concert</h2>
+                        <h1>Latest videos</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="youtube__item">
+                        <div class="youtube__item__pic set-bg" data-setbg="{{ asset('img/youtube/youtube-1.jpg') }}">
+                            <a href="https://www.youtube.com/watch?v=t5yyqXdhLjA" class="play-btn video-popup"><i class="fa fa-play"></i></a>
+                        </div>
+                        <div class="youtube__item__text">
+                            <h4>Imagine Dragons - I'm So Sorry - LIVE in Vegas
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="youtube__item">
+                        <div class="youtube__item__pic set-bg" data-setbg="{{ asset('img/youtube/youtube-2.jpg') }}">
+                            <a href="https://www.youtube.com/watch?v=4xqo7D2k8HM" class="play-btn video-popup"><i class="fa fa-play"></i></a>
+                        </div>
+                        <div class="youtube__item__text">
+                            <h4>Imagine Dragons - Believer (Live in Vegas)
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="youtube__item">
+                        <div class="youtube__item__pic set-bg" data-setbg="{{ asset('img/youtube/youtube-3.jpg') }}">
+                            <a href="https://www.youtube.com/watch?v=Fpn1imb9qZg" class="play-btn video-popup"><i class="fa fa-play"></i></a>
+                        </div>
+                        <div class="youtube__item__text">
+                            <h4>Coldplay - A Sky Full Of Stars (Live at River Plate)
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Youtube Section End -->
